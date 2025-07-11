1f9c25c872a3f9b670d4431be676b8cc6741b29d
dmitryreaper <dimadimof81@gmail.com>
2025-07-11 23:23:25 +0300

    update home nix env

--- nix/home.nix
+++ nix/home.nix
@@ -1,71 +1,81 @@
 { config, pkgs, ... }:
 
 {
-  # Базовая информация о пользователе
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
   home.username = "dima";
   home.homeDirectory = "/home/dima";
-  home.stateVersion = "24.11"; # Убедитесь, что версия совпадает с вашей системой
 
-  # Включение Home Manager
-  programs.home-manager.enable = true;
-  home.enableNixpkgsReleaseCheck = false;
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
-  # Установка пакетов
-  home.packages = with pkgs; [
-    git
-    zip
-    unzip
-    telegram-desktop
-    google-chrome
-    libreoffice-qt6-fresh
-    wpsoffice
-    spotify
-    jdk17
-    python3
-    btop
-    vscode
-    corefonts
-    vistafonts
-    obsidian
-    veracrypt
-    gimp
-    geteltorito
-    gcc
-    gnumake
-    gdb
-    rxvt-unicode
-    p7zip
-    wget
-    mono
-    llvmPackages_15.clang-tools
-    python312Packages.pip
-    python312Packages.python-lsp-server # Добавляем python-lsp-server
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
 
-    #virtualization
-    virt-manager
-    spice-gtk
+  	#applications
+	pkgs.telegram-desktop
+	pkgs.go
+	pkgs.gopls
 
-    #wm
-    polybar
-    feh
-    brightnessctl
-    picom-pijulius
-    rofi
-    sxhkd
-    terminus_font
+	#qt develop
+	pkgs.qtcreator
+	pkgs.libsForQt5.full
+	#pkgs.gcc
+	pkgs.gnumake
+	pkgs.cmake
+	
+	pkgs.clang
+	pkgs.clang-tools
+
+	#archivators
+	pkgs.unzip
+	pkgs.unrar
+	pkgs.p7zip
+
   ];
 
-  # Переменные окружения
-  home.sessionVariables = {
-    EDITOR = "emacs";
-    LANG = "en_US.UTF-8";
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
   };
 
-  # Управление файлами (пример)
-  home.file = {
-    ".bashrc".text = ''
-      export PATH="$HOME/.local/bin:$PATH"
-      xset r rate 300 100
-    '';
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/dima/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
   };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
