3eb013941422492eec4f972891bc88163cb7a569
dmitryreaper <dimadimof81@gmail.com>
2025-07-21 21:02:35 +0300

    not used

--- nix/home_modules/git.nix
+++
@@ -1,7 +0,0 @@
-{
-  programs.git = {
-    enable = true;
-    userName  = "dmitryreaper";
-    userEmail = "dimadimof81@gmail.com";
-  };
-}
--- nix/system_modules/users/user.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}:
-{
-  users.users.dima = {
-    isNormalUser = true;
-    description = "dima";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-}
