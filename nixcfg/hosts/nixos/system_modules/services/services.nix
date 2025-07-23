6d9c9d7a04d9894f7c139fcc50348b48467cd37a
dmitryreaper <dimadimof81@gmail.com>
2025-07-23 20:36:10 +0300

    update service postgresql

--- nixcfg/hosts/nixos/system_modules/services/services.nix
+++ nixcfg/hosts/nixos/system_modules/services/services.nix
@@ -1,13 +1,25 @@
 {pkgs, ...}:
 
 {
-  config.services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_15;
-    ensureDatabases = [ "mydatabase" ];
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
-    '';
-  };
+  services.postgresql = {
+  enable = true;
+  ensureDatabases = [ "mydb" ];
+  enableTCPIP = true;
+  # port = 5432;
+  authentication = pkgs.lib.mkOverride 10 ''
+    #...
+    #type database DBuser origin-address auth-method
+    local all       all     trust
+    # ipv4
+    host  all      all     127.0.0.1/32   trust
+    # ipv6
+    host all       all     ::1/128        trust
+  '';
+  # initialScript = pkgs.writeText "backend-initScript" ''
+  #   CREATE ROLE dima WITH LOGIN PASSWORD 'zxc011' CREATEDB;
+  #   CREATE DATABASE file_manager_db;
+  #   GRANT ALL PRIVILEGES ON DATABASE dima TO file_manager_db;
+  # '';
+};
+  
 }
