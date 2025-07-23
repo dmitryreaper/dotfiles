6136a4a8924562ac67543bd21cce7b33c8d76a26
dmitryreaper <dimadimof81@gmail.com>
2025-07-23 20:34:08 +0300

    add: wm xmonad

--- nixcfg/hosts/nixos/system_modules/graphics/x11/xserver.nix
+++ nixcfg/hosts/nixos/system_modules/graphics/x11/xserver.nix
@@ -3,5 +3,11 @@
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
+
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+    };
+    windowManager.xmonad.config = builtins.readFile ./xmonad.hs;
   };
 }
