659f23794ace724af7d6d2be181240c7d37169b8
dmitryreaper <dimadimof81@gmail.com>
2025-07-25 17:43:54 +0300

    new alias

--- nixcfg/home/dima/home_modules/bash.nix
+++ nixcfg/home/dima/home_modules/bash.nix
@@ -7,12 +7,14 @@
 
       sr = "sudo nixos-rebuild switch";
       sg = "nixos-rebuild list-generations";
+      re = "killall -s 9 emacs && emacs --daemon";
 
       em = "emacs -nw";
     };
     
     bashrcExtra = ''
       xset r rate 300 100
+      setxkbmap -option "ctrl:swapcaps" && setxkbmap -layout us,ru && setxkbmap -option 'grp:alt_shift_toggle'
     '';
   };
 }
